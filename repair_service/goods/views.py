49825279b7dbd60ccadb934cb20a9a19d89736db
programmistvdzinsahirybashke <ilnaz09.ys@gmail.com>
2024-10-15 23:10:34 +0300

    cats

--- repair_service/goods/views.py
+++ repair_service/goods/views.py
@@ -3,15 +3,19 @@ from goods.models import Service
 from lib2to3.fixes.fix_input import context
 
 # Create your views here.
-def catalog(request):
+def catalog(request, category_slug):
 
-    goods = Service.objects.all()
+    if category_slug == 'all':
+        goods = Service.objects.all()
+    else:
+        goods = Service.objects.filter(category__slug=category_slug)
+
 
     context = {
         'title':'Каталог услуг',
         'goods': goods,
     }
-    return render(request, 'goods/catalog.html', context)
+    return render(request, 'goods/catalog.html', context=context)
 
 def product(request, product_slug):
 
