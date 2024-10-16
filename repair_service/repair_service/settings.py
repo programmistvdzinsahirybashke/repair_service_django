8d5c2f1cbaf9ce751f340ddcb1584219522adf52
programmistvdzinsahirybashke <ilnaz09.ys@gmail.com>
2024-10-16 22:32:26 +0300

    1

--- repair_service/repair_service/settings.py
+++ repair_service/repair_service/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
-from django.conf.global_settings import STATICFILES_DIRS, MEDIA_URL, MEDIA_ROOT, INTERNAL_IPS
+from django.conf.global_settings import STATICFILES_DIRS, MEDIA_URL, MEDIA_ROOT, INTERNAL_IPS, AUTH_USER_MODEL
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -149,3 +149,4 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'users.USER'
--- repair_service/users/models.py
+++ repair_service/users/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class User(AbstractUser):
+    class Meta:
+        db_table = 'users'
+        verbose_name = 'Пользователя'
+        verbose_name_plural = 'Пользователи'
+
+    image = models.ImageField(upload_to='users_image', blank=True, null=True, verbose_name = 'Аватар')
+
