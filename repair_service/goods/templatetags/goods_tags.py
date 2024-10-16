c38a9fde379c676c6f2c0e95bf7017bf4dc98591
programmistvdzinsahirybashke <ilnaz09.ys@gmail.com>
2024-10-16 11:34:32 +0300

    urlencode

--- repair_service/goods/templatetags/goods_tags.py
+++ repair_service/goods/templatetags/goods_tags.py
@@ -1,3 +1,4 @@
+from django.utils.http import urlencode
 from goods.models import Category
 from django import template
 
@@ -7,3 +8,9 @@ register = template.Library()
 @register.simple_tag()
 def tag_categories():
     return Category.objects.all()
+
+@register.simple_tag(takes_context=True)
+def change_params(context, **kwargs):
+    query = context['request'].GET.dict()
+    query.update(kwargs)
+    return urlencode(query)
--- repair_service/goods/views.py
+++ repair_service/goods/views.py
@@ -8,11 +8,20 @@ from django.core.paginator import Paginator
 def catalog(request, category_slug):
 
     page = request.GET.get('page', 1)
+    on_sale = request.GET.get('on_sale', None)
+    order_by = request.GET.get('order_by', None)
+
 
     if category_slug == 'all':
         goods = Service.objects.all()
     else:
         goods = get_list_or_404(Service.objects.filter(category__slug=category_slug))
+
+    if on_sale:
+        goods = goods.filter(discount__gt=0)
+    if order_by and order_by != 'default':
+        goods = goods.order_by(order_by)
+
 
     paginator = Paginator(goods, per_page=3)
     current_page = paginator.page(page)
