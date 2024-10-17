76581ffebd8149f9d8aab7e532c25c6733e1de0d
programmistvdzinsahirybashke <ilnaz09.ys@gmail.com>
2024-10-17 12:32:35 +0300

    login

--- repair_service/users/views.py
+++ repair_service/users/views.py
@@ -1,9 +1,28 @@
+from django.contrib import auth
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+from users.forms import UserLoginForm
+
 
 # Create your views here.
 def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('repair_app:index'))
+    else:
+        form = UserLoginForm()
+
     context = {
-        'title': 'Вход'
+        'title': 'Вход',
+        'form': form,
     }
     return render(request, 'users/login.html', context=context)
 
