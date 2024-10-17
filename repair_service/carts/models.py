9cffd2e4c8cb42fd2eb6bf82938415eae67a18bc
programmistvdzinsahirybashke <ilnaz09.ys@gmail.com>
2024-10-17 21:42:13 +0300

    working cart

--- repair_service/carts/models.py
+++ repair_service/carts/models.py
@@ -32,4 +32,4 @@ class Cart(models.Model):
         return round(self.product.sell_price() * self.quantity, 2)
 
     def __str__(self):
-        return f'Корзина {self.user.username} | Товар {self.product.name} | Количество {self.quantity}'
+        return f'Корзина {self.user.username} | Товар {self.product.service_name} | Количество {self.quantity}'
--- repair_service/carts/views.py
+++ repair_service/carts/views.py
@@ -1,14 +1,30 @@
 from django.contrib.admin.templatetags.admin_list import pagination
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_list_or_404, redirect
 from lib2to3.fixes.fix_input import context
 from django.core.paginator import Paginator
 
+from goods.models import Service
+
+from carts.models import Cart
+
+
 # Create your views here.
-def cart_add(request, product_id):
+def cart_add(request, product_slug):
+    product = Service.objects.get(slug=product_slug)
+
+    if request.user.is_authenticated:
+        carts = Cart.objects.filter(user=request.user, product=product)
+        if carts.exists():
+            cart = carts.first()
+            if cart:
+                cart.quantity += 1
+                cart.save()
+        else:
+            Cart.objects.create(user=request.user, product=product, quantity=1)
+    return redirect(request.META['HTTP_REFERER'])
+
+def cart_change(request, product_slug):
     pass
 
-def cart_change(request, product_id):
-    pass
-
-def cart_remove(request, product_id):
+def cart_remove(request, product_slug):
     pass
