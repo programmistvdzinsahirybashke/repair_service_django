acdc2035026eca0888e464c3a709d9ac9983680c
programmistvdzinsahirybashke <ilnaz09.ys@gmail.com>
2024-10-18 13:30:07 +0300

    ajax cart add

--- repair_service/carts/views.py
+++ repair_service/carts/views.py
@@ -1,16 +1,21 @@
 from django.contrib.admin.templatetags.admin_list import pagination
+from django.http import JsonResponse
 from django.shortcuts import render, get_list_or_404, redirect
+from django.template.loader import render_to_string
 from lib2to3.fixes.fix_input import context
 from django.core.paginator import Paginator
 
+from carts.utils import get_user_carts
 from goods.models import Service
 
 from carts.models import Cart
 
 
 # Create your views here.
-def cart_add(request, product_slug):
-    product = Service.objects.get(slug=product_slug)
+def cart_add(request):
+
+    product_id = request.POST.get("product_id")
+    product = Service.objects.get(id=product_id)
 
     if request.user.is_authenticated:
         carts = Cart.objects.filter(user=request.user, product=product)
@@ -21,12 +26,20 @@ def cart_add(request, product_slug):
                 cart.save()
         else:
             Cart.objects.create(user=request.user, product=product, quantity=1)
-    return redirect(request.META['HTTP_REFERER'])
+
+    user_cart = get_user_carts(request)
+    cart_items_html = render_to_string(
+        "carts/includes/included_cart.html", {"carts": user_cart}, request=request)
+    response_data = {
+        "message": "Товар добавлен в корзину",
+        "cart_items_html": cart_items_html,
+    }
+    return JsonResponse(response_data)
 
 def cart_change(request, product_slug):
     pass
 
-def cart_remove(request, cart_id):
+def cart_remove(request):
 
     cart = Cart.objects.get(id=cart_id)
     cart.delete()
