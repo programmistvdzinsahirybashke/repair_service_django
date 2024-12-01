92d36eff1936b2bc73e787d6ad1056e5a9192ba5
programmistvdzinsahirybashke <ilnaz09.ys@gmail.com>
2024-12-01 21:59:19 +0300

    index button

--- repair_service/settings.py
+++ repair_service/settings.py
@@ -157,23 +157,4 @@ AUTH_USER_MODEL = 'users.USER'
 LOGIN_URL = '/user/login/'
 
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-        },
-        '__main__': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-        },
-    },
-}
+
